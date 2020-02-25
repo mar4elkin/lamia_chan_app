00b6b2022101515854a0f0e363aa19b615696c10
mark Тамаров <googlcraftlol@gmail.com>
2020-02-26 00:23:46 +0300

    Working on detail screen chapters

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { FlatList, ActivityIndicator, Text, View, Image, StyleSheet, Button } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { FlatList, ActivityIndicator, Text, View, Image, StyleSheet, ScrollView, SafeAreaView   } from 'react-native';
 
 import {
   NavigationContainer,
@@ -9,10 +9,10 @@ import {
 import { createStackNavigator } from '@react-navigation/stack';
 
 function HomeScreen() {
-  return <FetchExample />;
+  return <MangaHomeScreen />;
 }
 
-class FetchExample extends React.Component {
+class MangaHomeScreen extends React.Component {
   static contextType = NavigationContext;
 
   constructor(props){
@@ -73,16 +73,61 @@ class FetchExample extends React.Component {
   }
 }
 
-function MangaScreen({ route, navigation }) {
 
+function DetailScreen({ route, navigation }) {
+
+  const [hasError, setErrors] = useState(false);
+  const [mangaDarray, setmangaDarray] = useState({});
+  
   const { mangaId } = route.params;
 
+  const [tagsArrTitle, settagsArrTitle] = useState({});
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch("http://192.168.88.186:8000/api/v1/manga/" + mangaId);
+      res
+        .json()
+        .then(res => setmangaDarray(res))
+        .catch(err => setErrors(err));
+    }
+
+    fetchData();
+  });
+
+  /* 
+    tagsList
+    <FlatList style={{flexDirection:'row', flexWrap:'wrap'}} data={tagsArrTitle.tags}
+      renderItem={({item}) => <Text style={styles.DetailTagsText}>{item}</Text>}
+    />
+  
+  */
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text> { JSON.stringify(mangaId) } </Text>
-    </View>
+    <SafeAreaView    style={styles.DetailContainer}>
+        <ScrollView >
+        <Text style={styles.DetailtextView}>{mangaDarray.title}</Text>
+        <Image 
+              source = {{ uri: mangaDarray.preview_image_url }} 
+              style={styles.DetailImageView} 
+        />
+
+        <Text style={styles.DetailDescriptionText}> {mangaDarray.description} </Text>
+
+        <FlatList style={styles.mangaChaptersFlat} data={mangaDarray.chapter_set}
+          renderItem={({item}) => <View style={styles.DetailChaptersView}>
+              <Text style={styles.DetailChaptersText}>{item.title}
+              {"                                    "}
+              {item.updated.split('T', '1')}</Text>
+            </View>}
+        />
+
+      </ScrollView >
+    </SafeAreaView  >
   );
 }
+
+
 
 const Stack = createStackNavigator();
 
@@ -90,7 +135,7 @@ function App() {
   return (
       <Stack.Navigator initialRouteName="Каталог">
         <Stack.Screen name="Каталог" component={HomeScreen} />
-        <Stack.Screen name="Манга" component={MangaScreen} />
+        <Stack.Screen name="Манга" component={DetailScreen} />
       </Stack.Navigator>
   );
 }
@@ -106,6 +151,62 @@ const styles = StyleSheet.create({
       margin: 5,
       marginTop: (Platform.OS === 'ios') ? 5 : 0,
   
+  },
+
+  DetailContainer :{
+    justifyContent: 'center',
+
+  },
+
+  DetailChaptersText :{
+    justifyContent: 'center',
+    padding:3,
+    marginTop: 5,
+    marginLeft: 25,
+  
+  },
+
+  DetailDescriptionText: {
+    textAlign:'left',
+    width: '80%',
+    marginLeft: 35,
+    marginBottom: 20,
+  },
+
+  DetailChaptersView: {
+    marginLeft: 10,
+    borderColor: 'rgba(0, 0, 0, 0.7)',
+    borderBottomWidth: 0.5,
+    width: '90%',
+  },
+
+  mangaChaptersFlat : {
+    borderColor: 'rgba(0, 0, 0, 0.7)',
+    borderTopWidth: 1,
+  },
+
+
+  DetailtextView: {
+      textAlign:'center',
+      padding:3,
+      color: 'white',
+      width: '80%',
+      height: 49,
+      left: '10%',
+      backgroundColor: 'rgba(0, 0, 0, 0.7)',
+      marginBottom:15,
+      marginTop:15,
+  },
+
+  DetailImageView: {
+
+      width: '75%',
+      height: 450 ,
+      margin: 7,
+      borderRadius : 7,
+      marginBottom:15,
+      marginTop: 15,
+      marginLeft:50,
   },
   
   imageView: {
